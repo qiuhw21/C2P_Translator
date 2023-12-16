3e9c99fa6e72d5184ae5cdeaf4f51536cf8e9c30
liubf <cfantsu@gmail.com>
2023-12-16 10:41:08 +0800

    feat: complete c_parser.py

--- c_parser.py
+++ c_parser.py
@@ -1,5 +1,16 @@
 import ply.yacc as yacc
 from lexer import tokens  # Assuming you have a lexer file named 'lexer.py'
+import json
+
+precedence = (
+    ('left', 'OR'),                  # Lowest precedence
+    ('left', 'AND'),
+    ('nonassoc', 'EQ', 'NEQ'),
+    ('nonassoc', 'LT', 'LTE', 'GT', 'GTE'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE', 'MOD'),
+    ('right', 'UMINUS', 'NOT'),      # Highest precedence
+)
 
 # Utility function for parsing error
 def p_error(p):
@@ -83,7 +94,9 @@ def p_statement(p):
                  | compound_statement
                  | selection_statement
                  | iteration_statement
-                 | return_statement'''
+                 | return_statement
+                 | var_declaration
+                 | empty'''
     p[0] = p[1]
 
 # Expressions
@@ -94,6 +107,7 @@ def p_expression_statement(p):
 def p_expression(p):
     '''expression : assignment_expression
                   | binary_expression
+                  | unary_expression
                   | term'''
     p[0] = p[1]
 
@@ -105,19 +119,51 @@ def p_binary_expression(p):
     '''binary_expression : expression PLUS expression
                          | expression MINUS expression
                          | expression TIMES expression
-                         | expression DIVIDE expression'''
+                         | expression DIVIDE expression
+                         | expression MOD expression
+                         | expression EQ expression
+                         | expression NEQ expression
+                         | expression LT expression
+                         | expression GT expression
+                         | expression LTE expression
+                         | expression GTE expression
+                         | expression AND expression
+                         | expression OR expression'''
     p[0] = ('binary_expression', p[2], p[1], p[3])
+
+def p_unary_expression(p):
+    '''unary_expression : MINUS expression %prec UMINUS
+                        | NOT expression
+                        | INC IDENTIFIER
+                        | DEC IDENTIFIER'''
+    p[0] = ('unary_expression', p[1], p[2])
 
 def p_term(p):
     '''term : IDENTIFIER
             | INT_LITERAL
-            | LPAREN expression RPAREN'''
+            | LPAREN expression RPAREN
+            | function_call'''
     p[0] = ('term', p[1])
+
+def p_function_call(p):
+    'function_call : IDENTIFIER LPAREN argument_list RPAREN'
+    p[0] = ('function_call', p[1], p[3])
+
+def p_argument_list(p):
+    '''argument_list : argument_list COMMA expression
+                     | expression
+                     | empty'''
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    elif len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = []
 
 # Control Structures
 def p_selection_statement(p):
-    '''selection_statement : IF LPAREN expression RPAREN statement
-                           | IF LPAREN expression RPAREN statement ELSE statement'''
+    '''selection_statement : IF LPAREN expression RPAREN compound_statement
+                           | IF LPAREN expression RPAREN compound_statement ELSE compound_statement'''
     if len(p) == 6:
         p[0] = ('if_statement', p[3], p[5])
     else:
@@ -136,23 +182,23 @@ def p_return_statement(p):
     p[0] = ('return_statement', p[2])
 
 # Build the parser
-parser = yacc.yacc()
+parser = yacc.yacc(start='program')
 
-# Test the parser
-def parse(data):
+# Function to parse and return JSON
+def parse_to_json(data):
     result = parser.parse(data)
-    return result
+    print(result)
+    return json.dumps(result, indent=4)
 
 # Example usage
 input_data = '''
 int main() {
-    int x = 10;
+    int x;
     if (x > 5) {
         return x;
-    } else {
-        return 0;
     }
+    return 0;
 }
 '''
-ast = parse(input_data)
+ast = parse_to_json(input_data)
 print(ast)
